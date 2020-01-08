ee02c5d23db499fe0572b21df637db77f5ecb516
Sarawut Wongsanga <ecs.sarawut@gmail.com>
2020-01-08 09:31:54 +0700

    .

---
+++ single-page/countdown/static/js/calculate_age.js
@@ -0,0 +1,45 @@
+    // Set the date we're counting down to
+    var BirthDate = new Date("July 03, 2014 15:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = now-BirthDate;
+
+      function zeroPadding(num, digit) {
+        var zero = '';
+        for (var i = 0; i < digit; i++) {
+          zero += '0';
+        }
+        return (zero + num).slice(-digit);
+      }
+
+      // Time calculations for days, hours, minutes and seconds
+      var year_age = Math.floor(distance / 31536000000);
+      
+      var day_age = Math.floor((distance % 31536000000) / 86400000);
+      var month_age = Math.floor(day_age / 30);
+
+        day_age = day_age % 30;
+      var hour_age =  zeroPadding(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),2);
+      var minute_age = zeroPadding(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),2);
+      var second_age = zeroPadding(Math.floor((distance % (1000 * 60)) / 1000),2);
+
+      // Output the result in an element with id="demo"
+      // document.getElementById("demo").innerHTML = days + " วัน " + hours + " ชั่วโมง "
+      //   + minutes + " นาที " + seconds + " วินาที ";
+      document.getElementById("year_age").innerHTML = year_age;
+      document.getElementById("month_age").innerHTML = month_age;
+      document.getElementById("day_age").innerHTML = day_age;
+      document.getElementById("hour_age").innerHTML = hour_age;
+      document.getElementById("minute_age").innerHTML = minute_age;
+      document.getElementById("second_age").innerHTML = second_age;
+     
+
+      // If the count down is over, write some text 
+     
+    }, 1000);
---
+++ single-page/countdown/static/js/script.js
@@ -0,0 +1,80 @@
+    // Set the date we're counting down to
+    var countDownDate = new Date("Jan 11, 2020 08:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      function zeroPadding(num, digit) {
+        var zero = '';
+        for (var i = 0; i < digit; i++) {
+          zero += '0';
+        }
+        return (zero + num).slice(-digit);
+      }
+
+      // Time calculations for days, hours, minutes and seconds
+      var days =  Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours =  zeroPadding(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),2);
+      var minutes = zeroPadding(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),2);
+      var seconds = zeroPadding(Math.floor((distance % (1000 * 60)) / 1000),2);
+
+      // Output the result in an element with id="demo"
+      // document.getElementById("demo").innerHTML = days + " วัน " + hours + " ชั่วโมง "
+      //   + minutes + " นาที " + seconds + " วินาที ";
+      document.getElementById("days").innerHTML = days;
+      document.getElementById("hours").innerHTML = hours;
+      document.getElementById("minutes").innerHTML = minutes;
+      document.getElementById("seconds").innerHTML = seconds;
+      document.getElementById("demo").innerHTML = '(' + days +'วัน) '+ hours + ":" + minutes + ":" + seconds;
+
+      // If the count down is over, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        
+        
+        document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+    var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+      "November", "December"
+    ];
+
+
+    var timerID = setInterval(updateTime, 1000);
+    updateTime();
+
+    function updateTime() {
+
+      document.getElementById("time_now").innerHTML = "";
+      var tnow = new Date();
+      var today = tnow.toDateString();
+      var time = tnow.toLocaleTimeString();
+      var hrs = zeroPadding(tnow.getHours(), 2);
+      var min = zeroPadding(tnow.getMinutes(), 2);
+      var sec = zeroPadding(tnow.getSeconds(), 2);
+
+      var day_names = week[tnow.getDay()];
+      var month_nows = months[tnow.getMonth()];
+      var date_nows = tnow.getDate();
+
+      function zeroPadding(num, digit) {
+        var zero = '';
+        for (var i = 0; i < digit; i++) {
+          zero += '0';
+        }
+        return (zero + num).slice(-digit);
+      }
+
+      document.getElementById("day_name").innerHTML = day_names;
+      document.getElementById("month_now").innerHTML = month_nows;
+      document.getElementById("date_now").innerHTML = date_nows;
+
+      document.getElementById("time_now").innerHTML = hrs + ':' + min + ':' + sec;
+    }
