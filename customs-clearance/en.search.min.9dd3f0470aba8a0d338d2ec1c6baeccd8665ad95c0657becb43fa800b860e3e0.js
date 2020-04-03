31de2458d1e2f60be64bb7d2044d64cb38bdf731
Sarawut Wongsanga <ecs.sarawut@gmail.com>
2020-04-03 12:15:32 +0700

    .

---
+++ customs-clearance/en.search.min.9dd3f0470aba8a0d338d2ec1c6baeccd8665ad95c0657becb43fa800b860e3e0.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/knowledge-center/customs-clearance/flexsearch.min.js');loadScript('/knowledge-center/customs-clearance/en.search-data.min.a05e611b44f5b3335e1c08c10f26c0c2663c6d1cb59bcf29c7d74db50565ee29.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
