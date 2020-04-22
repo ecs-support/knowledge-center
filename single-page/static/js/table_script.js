0959bc7c65756517001a4ea9eee9d2b5431ca915
Sarawut Wongsanga <ecs.sarawut@gmail.com>
2020-04-22 10:18:55 +0700

    .

--- single-page/static/js/table_script.js
+++ single-page/static/js/table_script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   var table = $("#table-ref").DataTable({
     responsive: true,
+    scroller: true,
+    scrollY: 600,
+    scrollX: 700,
   });
 
   new $.fn.dataTable.FixedHeader(table);
